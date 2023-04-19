feeccdd48eb3b1b5ba7c4024a08d19d6744a4301
Haresh-thumar <hareshthumar46@gmail.com>
2023-04-20 01:58:15 +0530

    concat & merge operator example

--- src/app/observable/concat/concat.component.ts
+++ src/app/observable/concat/concat.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { concat, interval, map, take } from 'rxjs';
+import { DesignUtilityService } from 'src/app/services/design-utility.service';
+import { UtilityOperatorService } from 'src/app/services/utility-operator.service';
 
 @Component({
   selector: 'app-concat',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConcatComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _du: UtilityOperatorService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  subscribeBtn: boolean = false;
+  example1() {
+    const sourceTech = interval(1000).pipe(map(v1 => 'Tech Video : ' + (v1 + 1)), take(5));
+    const sourceComedy = interval(2000).pipe(map(v2 => 'Comedy Video : ' + (v2 + 1)), take(3));
+    const sourceNews = interval(500).pipe(map(v3 => 'News Video : ' + (v3 + 1)), take(4));
+
+    const result = concat(sourceTech, sourceComedy, sourceNews);
+
+    result.subscribe(res => {
+      this._du.printLi(res, 'el-container');
+    });
   }
 
 }
--- src/app/observable/merge/merge.component.ts
+++ src/app/observable/merge/merge.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, map, merge, take } from 'rxjs';
+import { UtilityOperatorService } from 'src/app/services/utility-operator.service';
 
 @Component({
   selector: 'app-merge',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MergeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _du: UtilityOperatorService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  subscribeBtn: boolean = false;
+  example1() {
+    const sourceTech = interval(3000).pipe(map(v1 => 'Tech Video : ' + (v1 + 1)), take(5));
+    const sourceComedy = interval(6000).pipe(map(v2 => 'Comedy Video : ' + (v2 + 1)), take(3));
+    const sourceNews = interval(3500).pipe(map(v3 => 'News Video : ' + (v3 + 1)), take(4));
+
+    const result = merge(sourceTech, sourceComedy, sourceNews);
+
+    result.subscribe(res => {
+      this._du.printLi(res, 'el-container');
+    });
   }
 
 }
